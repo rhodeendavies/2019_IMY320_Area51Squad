3ed2f17b073a4ae954334fa562e52f3ae9edd647
RaniArraf <arraf.rani@gmail.com>
2019-09-09 00:42:47 +0200

    Cursor red

--- js/script.js
+++ js/script.js
@@ -69,3 +69,71 @@ $(()=>{
 	
 
 });
+
+
+$("body").append('<div class="cursor"></div>');
+var land = document.getElementById("contact").id;
+var con = document.getElementById("contact").id;
+if(con == "contact")
+{	
+	$("html").append(
+	   "<style>html, body, .msty_notcur {cursor:none !important;}.cursor {z-index:10000000000000; position: fixed;background-color:rgba(255,0,0,0.4);border: 3px solid rgba(255,255,255,0.4);width:30px;height:30px;border-radius:100%;transform:translate(-50%,-50%);top:0px;left:0px;pointer-events:none;} .overlink {background-color:rgba(255,100,100,0.1) !important;border: 1px solid rgba(100,0,0,0.25) !important;} .overtext {background-color:rgba(100,100,255,0.25) !important;border: 1px solid rgba(0,0,100,0.25) !important;}</style>"
+	);
+}
+else if(land == "landing")
+{
+	$("html").append(
+	   "<style>html, body, .msty_notcur {cursor:none !important;}.cursor {z-index:10000000000000; position: fixed;background-color:rgba(255,255,255,0.4);border: 3px solid rgba(255,255,255,0.4);width:30px;height:30px;border-radius:100%;transform:translate(-50%,-50%);top:0px;left:0px;pointer-events:none;} .overlink {background-color:rgba(255,100,100,0.1) !important;border: 1px solid rgba(100,0,0,0.25) !important;} .overtext {background-color:rgba(100,100,255,0.25) !important;border: 1px solid rgba(0,0,100,0.25) !important;}</style>"
+	);
+}
+var scrollY = 0,
+   scrollX = 0;
+$(document).mousemove(function(e) {
+   $(".cursor")
+      .css("top", e.pageY - scrollY + "px")
+      .css("left", e.pageX - scrollX + "px");
+});
+$(document).scroll(function(e) {
+   scrollY = $(window).scrollTop();
+   scrollX = $(window).scrollLeft();
+});
+setInterval(function() {
+   scroll = $(window).scrollTop();
+}, 1000);
+$("*").hover(
+   function(e) {
+      var index = -1;
+      try {
+         index = $(this)
+            .attr("class")
+            .toLowerCase()
+            .indexOf("button");
+         if (index == -1) {
+            index = $(this)
+               .attr("class")
+               .toLowerCase()
+               .indexOf("link");
+         }
+      } catch (e) {
+         index = -1;
+      }
+      if (
+         $(this).css("cursor") == "pointer" ||
+         $(this).get(0).tagName == "A" ||
+         $(this).get(0).tagName == "BUTTON" ||
+         $(this).hasClass("msty_cur") ||
+         index > -1
+      ) {
+         $(this).addClass("msty_cur");
+         $(this).css("cursor", "none");
+         $(".cursor").addClass("overlink");
+      }
+      if ($(this).css("cursor") != "none") {
+         $(this).addClass("msty_notcur");
+      }
+   },
+   function(e) {
+      $(this).css("cursor", "none");
+      $(".cursor").removeClass("overlink");
+   }
+);
