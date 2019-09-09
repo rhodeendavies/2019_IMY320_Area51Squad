173f110729aab5fe2d756b282f24d4670aa3dfbe
rhodeendavies <49963016+rhodeendavies@users.noreply.github.com>
2019-09-09 22:18:46 +0200

    Foreground divs transform
    Animation for foreground moving in and out when scrolling

--- js/script.js
+++ js/script.js
@@ -6,26 +6,25 @@ $(()=>{
 
 	$('html, body').css("background-color", "#202321");
 
-	var activeColour = 0;
-
 	//function to scroll page
 	function scroll(){
 		let distance = $('.activePage').offset().top;
 
 		var currScrollTop = $(window).scrollTop();
 		//scrolling down
-		if (currScrollTop > lastScrollTop){
-			
+		if (currScrollTop > lastScrollTop){	
 			if ($(window).scrollTop() > distance){
-				++activeColour;
 				$(window).off('scroll');
 
 				let oldActive = $('.activePage').first();
 				oldActive.removeClass('activePage');
+				oldActive.find('.rightFGActive').removeClass('rightFGActive');
+				oldActive.find('.leftFGActive').removeClass('leftFGActive');
+
 				let newActive = oldActive.next();
 				newActive.addClass('activePage');
-
-				console.log(newActive.attr('data-backgroundColour'));
+				newActive.find('.rightFG').addClass('rightFGActive');
+				newActive.find('.leftFG').addClass('leftFGActive');
 
 				$('html, body').animate({
 			        scrollTop: newActive.offset().top,
@@ -41,13 +40,17 @@ $(()=>{
 		//scrolling up
 		} else {
 			if ($(window).scrollTop() < distance){
-				--activeColour;
 				$(window).off('scroll');
 
 				let oldActive = $('.activePage').first();
 				oldActive.removeClass('activePage');
+				oldActive.find('.rightFGActive').removeClass('rightFGActive');
+				oldActive.find('.leftFGActive').removeClass('leftFGActive');
+
 				let newActive = oldActive.prev();
 				newActive.addClass('activePage');
+				newActive.find('.rightFG').addClass('rightFGActive');
+				newActive.find('.leftFG').addClass('leftFGActive');
 
 				$('html, body').animate({
 			        scrollTop: newActive.offset().top,
