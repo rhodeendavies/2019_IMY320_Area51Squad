6aefc9d5cf870352b238c9d204d3f2df403f2a2c
rhodeendavies <49963016+rhodeendavies@users.noreply.github.com>
2019-10-09 21:52:45 +0200

    parallax fix

--- js/script.js
+++ js/script.js
@@ -10,9 +10,17 @@ $(()=>{
 	let lastScrollTop = $(window).scrollTop();
 	let oldActive = $('.activePage');
 
+	$(window).scrollTop(0);
 
-	//scroll
 
+	 $('[data-toggle="tooltip"]').tooltip();
+	
+
+	const leftValue = parseFloat($('#split .leftFG').css('left').substr(0, $('#split .leftFG').css('left').length - 2));
+	const rightValue = parseFloat($('#split .rightFG').css('right').substr(0, $('#split .rightFG').css('right').length - 2));
+
+
+	//is element in viewport
 	$.fn.isInViewport = function() {
 		var elementTop = $(this).offset().top;
 		var elementBottom = elementTop + $(this).outerHeight();
@@ -23,23 +31,25 @@ $(()=>{
 		return elementBottom > viewportTop && elementTop < viewportBottom;
 	};
 
+	//scroll
 	function scrollAnimation(){
-		if (lastScrollTop < $(window).scrollTop()){
-			if (!$('.activePage').isInViewport()){
-				
-				oldActive.removeClass('activePage');
+		if (!$('.activePage').isInViewport()){
+			oldActive.removeClass('activePage');
+			oldActive.find('leftFG').removeClass('leftFGActive');
+			oldActive.find('rightFG').removeClass('rightFGActive');
+
+			if (lastScrollTop < $(window).scrollTop())
 				oldActive.next().addClass('activePage');
-				oldActive = $('.activePage');
-			}
-			
-		} else {
-			if (!$('.activePage').isInViewport()){
-				
-				oldActive.removeClass('activePage');
+			else
 				oldActive.prev().addClass('activePage');
-				oldActive = $('.activePage');
-			}
+
+			oldActive = $('.activePage');
+			oldActive.find('leftFG').addClass('leftFGActive');
+			oldActive.find('rightFG').addClass('rightFGActive');	
 		}
+
+
+
 		let otherPage;
 		//calculate distance between two pages, based on which pages are in view
 		if (oldActive.next().isInViewport())
@@ -47,7 +57,7 @@ $(()=>{
 		else
 			otherPage = oldActive.prev();
 
-
+		//COLOR
 		//get color changing from and to in array [red, green, blue]
 		let oldActiveColor = [oldActive.attr('data-red'), 
 							oldActive.attr('data-green'), 
@@ -63,21 +73,71 @@ $(()=>{
 
 		let percentToNextPage;
 		//if active page is above the other page
-		if (oldActive.offset().top < otherPage.offset().top){
-			percentToNextPage = (otherPage.offset().top - $(window).scrollTop())/ $(window).height() * 100;
-			newColor.forEach((item, index) => {
-				newColor[index] = item * (100 - percentToNextPage)/ 100;
-			});
-			
+		if (oldActive.offset().top < otherPage.offset().top)
+			percentToNextPage = 100 - (otherPage.offset().top - $(window).scrollTop())/ $(window).height() * 100;
 		//otherwise the other page is above the active page
-		} else {
+		else 
 			percentToNextPage = -1* ($(window).scrollTop() - oldActive.offset().top)/ $(window).height() * 100;
-			newColor.forEach((item, index) => {
-				newColor[index] = item * percentToNextPage/ 100;
-			});
-		}
+
+		newColor.forEach((item, index) => {
+			newColor[index] = item * percentToNextPage/ 100;
+		});
 
 		$('html, body').css("background-color", `rgb(${oldActiveColor[0] - newColor[0]},${oldActiveColor[1] - newColor[1]},${oldActiveColor[2] - newColor[2]})`);
+
+		console.log(percentToNextPage)
+
+
+		//PARALLAX
+		if (percentToNextPage > 20){
+			if (otherPage.find('.leftFG').length){
+				let leftFG = otherPage.find('.leftFG');
+				let rightFG = otherPage.find('.rightFG');
+				
+				leftFG.css('left', `${leftValue - (leftValue * percentToNextPage/ 100)}px`);
+				rightFG.css('right', `${rightValue - (rightValue * percentToNextPage/ 100)}px`);
+				
+				leftFG.css('transform', `rotate(${-20 - (-20 * percentToNextPage/ 100)}deg)`);
+				rightFG.css('transform', `rotate(${20 - (20 * percentToNextPage/ 100)}deg)`);				
+			}
+
+			leftFG = oldActive.find('.leftFG');
+			rightFG = oldActive.find('.rightFG');
+			
+			leftFG.css('left', `${leftValue * percentToNextPage/ 100}px`);
+			rightFG.css('right', `${rightValue * percentToNextPage/ 100}px`);
+
+			leftFG.css('transform', `rotate(${-20 * percentToNextPage/ 100}deg)`);
+			rightFG.css('transform', `rotate(${20 * percentToNextPage/ 100}deg)`);		
+
+		} else {
+			if (otherPage.find('.leftFG').length){
+				let leftFG = otherPage.find('.leftFG');
+				let rightFG = otherPage.find('.rightFG');
+				
+				leftFG.css('left', `${leftValue}px`);
+				rightFG.css('right', `${rightValue}px`);
+				
+				leftFG.css('transform', `rotate(-20deg)`);
+				rightFG.css('transform', `rotate(20deg)`);				
+			}
+
+			leftFG = oldActive.find('.leftFG');
+			rightFG = oldActive.find('.rightFG');
+			
+			leftFG.css('left', `0px`);
+			rightFG.css('right', `0px`);
+
+			leftFG.css('transform', `rotate(0deg)`);
+			rightFG.css('transform', `rotate(0deg)`);		
+		}
+
+		
+		
+		
+
+
+
 		lastScrollTop = $(window).scrollTop();
 		
 	}
@@ -110,8 +170,6 @@ $(()=>{
 
 	//parallax event listener
 	$(window).on('mousemove', parallax);
-
-
 
 
 	//mouse pointer
